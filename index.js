94ff44b8a57d4470c8b99f81ed20055ed0d114ac
Johan Palmfjord <johan.palmfjord@gmail.com>
2017-11-11 13:04:06 +0100

    Add more fixtures and add voting functionality

--- index.js
+++ index.js
@@ -8,7 +8,25 @@ app.use(bodyParser.json())
 const blabbers = [
   {
     id: 'd9f91448-6da4-4dfb-bd17-6cbf290f4a3b',
-    text: 'blabber blabber',
+    text: 'If I won the award for laziness, I would send somebody to pick it up for me.',
+    votes: 12,
+    comments: [
+      {
+        id: 'c66602de-5da6-4da8-8780-4b688d05c002',
+        text: 'Looool',
+      }
+    ]
+  },
+  {
+    id: 'c850f5fe-6e40-4829-95e3-56f882617783',
+    text: 'Don\'t know where your kids are in the house? Turn off the internet and they\'ll show up quickly.',
+    votes: -3,
+    comments: []
+  },
+  {
+    id: 'eb1c2926-b8cf-40ef-88d6-84a386bfa76e',
+    text: 'My bed is a magical place where I suddenly remember everything I forgot to do.',
+    votes: 0,
     comments: []
   }
 ]
@@ -16,6 +34,7 @@ const createBlabber = text => {
   const newBlabber = {
     id: uuid(),
     text,
+    votes: 0,
     comments: []
   }
   blabbers.push(newBlabber)
@@ -55,6 +74,7 @@ app.post('/blabbers', ({ body }, res) => {
   if (!body.text) {
     res.statusCode = 400
     res.send({ error: 'TEXT_REQUIRED' })
+    return
   }
 
   if (body.text.length > 600) {
@@ -88,6 +108,7 @@ app.post('/blabbers/:id/comments', ({ params: { id }, body }, res) => {
   if (!body.text) {
     res.statusCode = 400
     res.send({ error: 'TEXT_REQUIRED' })
+    return
   }
 
   if (body.text.length > 600) {
@@ -99,6 +120,34 @@ app.post('/blabbers/:id/comments', ({ params: { id }, body }, res) => {
   const comment = addCommentToBlabber(id, body.text)
   res.statusCode = 201
   res.send(comment)
+})
+
+app.post('/blabbers/:id/votes', ({ params: { id } }, res) => {
+  const blabber = getBlabberById(id)
+
+  if (!blabber) {
+    res.statusCode = 404
+    res.send()
+    return
+  }
+
+  blabber.votes += 1
+
+  res.send(blabber)
+})
+
+app.delete('/blabbers/:id/votes', ({ params: { id } }, res) => {
+  const blabber = getBlabberById(id)
+
+  if (!blabber) {
+    res.statusCode = 404
+    res.send()
+    return
+  }
+
+  blabber.votes -= 1
+
+  res.send(blabber)
 })
 
 
