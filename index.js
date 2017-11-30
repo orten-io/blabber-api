48119070e333805b424e2296a4c80c5d88ac7fa2
Johan Palmfjord <johan.palmfjord@gmail.com>
2017-11-30 18:03:58 +0100

    Add cors headers

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const uuid = require('uuid/v4')
+const cors = require('cors')
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cors())
 
 let blabbers = [
   {
@@ -63,15 +65,6 @@ app.use((err, req, res, next) => {
   }
 
   next(err)
-})
-
-
-// Add CORS headers
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  res.header("Access-Control-Allow-Methods", "OPTIONS, GET, POST, DELETE")
-  next()
 })
 
 app.get('/blabbers', (req, res) => {
